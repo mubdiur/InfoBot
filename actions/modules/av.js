a1e5d2b93b46b773fb95b6d9fd8a44f8654a8ada
Md. Mubdiur Rahman <mubdiur@gmail.com>
2021-06-18 22:30:52 +0600

    trying reply again instead of channel send in avatar

--- actions/modules/av.js
+++ actions/modules/av.js
@@ -4,14 +4,14 @@ module.exports = {
     helptext: "Shows avater of the user himself or a mentioned user",
     getReply: (msg, text) => {
         const user = msg.mentions.users.first() || msg.author;
-        const embed = new MessageEmbed()
+        return new MessageEmbed()
             .setColor('#333333')
             .setAuthor(user.username)
             .setImage(user.displayAvatarURL({
                 dynamic: true,
                 size: 1024
             }))
-        msg.channel.send(embed)
-        return null
+        // msg.channel.send(embed)
+        // return null
     }
 }
