ab5246302b6ebb3c64fb7042b18b9e3846a3f234
Md. Mubdiur Rahman <mubdiur@gmail.com>
2021-06-21 17:07:29 +0600

    different module for finduser

--- actions/modules/av.js
+++ actions/modules/av.js
@@ -1,56 +1,10 @@
 const { MessageEmbed } = require('discord.js')
+const finduser = require('./finduser');
 
 module.exports = {
     helptext: "Shows avater of the user himself or a mentioned user",
     getReply: async (msg, text) => {
-        const trimmed = text.trim().toLowerCase();
-        // if an id is given
-
-
-        let user = null;
-
-
-        user = msg.mentions.users.first();
-
-        // if user is null
-        if (!user) {
-            if (trimmed.length > 0) {
-                const membersObj = await msg.guild.members.fetch();
-                const membersLarge = JSON.parse(JSON.stringify(membersObj));
-                const members = [];
-                for await (member of membersLarge) {
-                    const fetchedMember = await msg.guild.members.fetch(member.userID);
-                    const tag = fetchedMember.user.tag;
-                    const tagpart = tag.toString().split('#')[0].toLowerCase()
-
-                    const memberItem = {
-                        id: member.userID,
-                        tag: fetchedMember.user.tag.toLowerCase(),
-                        tagpart: tagpart.toLowerCase(),
-                        nick: member.nickname ? member.nickname.toLowerCase() : null,
-                        name: member.displayName.toLowerCase()
-                    }
-                    members.push(memberItem)
-                }
-
-
-                for await (member of members) {
-                    // if the anything matched
-                    if (
-                        trimmed === member.id ||
-                        trimmed === member.tag ||
-                        trimmed === member.tagpart ||
-                        trimmed === member.nick ||
-                        trimmed === member.name
-                    ) {
-                        const matchedMember = await msg.guild.members.fetch(member.id);
-                        user = matchedMember.user;
-                    }
-                }
-            } else {
-                user = msg.author;
-            }
-        }
+        const user = await finduser(msg, text)
         let reply = "User not found in this server!"
         if (user) {
             reply =  new MessageEmbed()
