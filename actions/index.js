70caf910e3f897e1fb6a98413ad9a785b54862c5
Md. Mubdiur Rahman <mubdiur@gmail.com>
2021-06-22 18:02:51 +0600

    added image

--- actions/index.js
+++ actions/index.js
@@ -1,4 +1,4 @@
 module.exports['help'] = require('./modules/help');
 module.exports['ping'] = require('./modules/ping');
 module.exports['av'] = require('./modules/av');
-
+module.exports['image'] = require('./modules/image');
---
+++ actions/modules/image.js
@@ -0,0 +1,43 @@
+const gis = require('g-i-s');
+const { MessageEmbed } = require('discord.js');
+const Pagination = require('discord-paginationembed');
+
+module.exports = {
+     helptext: "Searches for images on the web",
+     getReply: async (msg, text) => {
+          if (text.trim().length > 0) {
+               /**
+                * Get the search result
+                */
+               gis(text, (error, results) => {
+                    if (error) {
+                         console.log(error);
+                    }
+                    else {
+                         const links = Array.from(results).map(each => each.url)
+                         /**
+                          * for now just show the first one
+                          */
+                         const embeds = [];
+                         let linkslen = 10;
+                         if (links.length < 10) linkslen = links.length
+                         for (let i = 0; i < links.length && i < 10; i++) {
+                              const embed = new MessageEmbed()
+                                   .setTitle(text + ` ${i + 1} of ${linkslen}`)
+                                   .setImage(links[i])
+                              embeds.push(embed)
+                         }
+                         new Pagination.Embeds()
+                              .setArray(embeds)
+                              .setChannel(msg.channel)
+                              .setDisabledNavigationEmojis(['delete', 'jump'])
+                              .setTimeout(180000)
+                              .build()
+
+                    }
+               });
+
+          }
+          return null;
+     }
+}
