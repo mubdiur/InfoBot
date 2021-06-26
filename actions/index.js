ae50ad1a9ce19c1a2a9a862500773a920fb3cb24
Md. Mubdiur Rahman <mubdiur@gmail.com>
2021-06-27 00:49:40 +0600

    added search functionality in index.js (actions)

--- actions/index.js
+++ actions/index.js
@@ -1,4 +1,5 @@
 module.exports['help'] = require('./modules/help');
 module.exports['ping'] = require('./modules/ping');
 module.exports['av'] = require('./modules/av');
+module.exports['search'] = require('./modules/search');
 module.exports['image'] = require('./modules/image');
