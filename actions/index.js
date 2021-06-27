2b94a22fa005f78c1a2b7ff31c9dac9a9256f8b8
Md. Mubdiur Rahman <mubdiur@gmail.com>
2021-06-27 22:51:31 +0600

    added define command

--- actions/index.js
+++ actions/index.js
@@ -3,3 +3,4 @@ module.exports['ping'] = require('./modules/ping');
 module.exports['av'] = require('./modules/av');
 module.exports['search'] = require('./modules/search');
 module.exports['image'] = require('./modules/image');
+module.exports['define'] = require('./modules/define');
---
+++ actions/modules/define.js
@@ -0,0 +1,73 @@
+const { MessageEmbed } = require('discord.js');
+const Pagination = require('discord-paginationembed');
+const unsafe = require('./unsafe')
+const Owl = require('owlbot-js')
+
+
+
+
+const dictionary = Owl(process.env.OWLAPI);
+
+
+module.exports = {
+    helptext: "Shows definitions",
+    getReply: async (msg, text) => {
+        if (unsafe(text)) {
+            msg.reply("It is unsafe to search that.");
+            return null;
+        }
+        if (text.trim().length > 0) {
+            let results = null;
+            let resArray = null;
+
+            try {
+                results = await dictionary.define(text);
+                resArray = Array.from(results.definitions);
+            } catch (e) { }
+            
+            if (!results || !resArray) {
+                msg.reply('The term was not found!');
+                return null;
+            }
+            
+            
+            
+            /**
+             * for now just show the first one
+             */
+            
+            
+            const embeds = [];
+            let resLen = 10;
+            if (resArray.length < 10) resLen = resArray.length
+            for (let i = 0; i < resArray.length && i < 10; i++) {
+                const embed = new MessageEmbed()
+                    .setAuthor(text + ` ${i + 1} of ${resLen}`)
+                    .addField('Definition', resArray[i].definition)
+                    .addField('Type', resArray[i].type);
+                if (resArray[i].example) {
+                    const example = resArray[i].example
+                        .split('<b>').join('')
+                        .split('</b>').join('');
+                    embed.addField('Example', "*`" + example + "`*")
+                }
+                embeds.push(embed)
+            }
+            if (embeds.length < 1) {
+                msg.reply('No results found for that search term!');
+            } else {
+                new Pagination.Embeds()
+                    .setArray(embeds)
+                    .setChannel(msg.channel)
+                    .setDisabledNavigationEmojis(['delete', 'jump'])
+                    .setNavigationEmojis({
+                        back: '⬅️',
+                        forward: '➡️'
+                    })
+                    .setTimeout(180000)
+                    .build()
+            }
+        }
+        return null;
+    }
+}
